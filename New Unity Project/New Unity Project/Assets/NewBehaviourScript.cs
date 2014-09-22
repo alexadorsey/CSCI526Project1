afb2f626751430b3d56b4a53e3c5410cc5b8e6c7
Anuja <anuja.chandorkar@gmail.com>
2014-09-22 14:48:59 -0700

    Initial commit

---
+++ New Unity Project/New Unity Project/Assets/NewBehaviourScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        rigidbody.isKinematic = true;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
