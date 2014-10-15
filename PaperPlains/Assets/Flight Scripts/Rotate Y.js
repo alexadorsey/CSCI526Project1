3d6618bb6feba96ec0821afb48eabf1e04285209
Chong Li <hero_chong@qq.com>
2014-10-14 20:36:57 -0700

    Revert 0aed01e..012cce6
    This rolls back to commit 0aed01e36f0f546bd511d60953ffd86094f84f93.

--- PaperPlains/Assets/Flight Scripts/Rotate Y.js
+++ PaperPlains/Assets/Flight Scripts/Rotate Y.js
@@ -325,13 +325,13 @@ function Update() {
 		// Character controls
 		if (!inCountdown){
 	    	var controller : CharacterController = GetComponent(CharacterController);
-	    	transform.Rotate(0, Input.GetAxis ("Horizontal") * rotateSpeed, 0);
-	    	var h = Input.GetAxis("Vertical"); // use the same axis that move back/forth
-	    	var v = Input.GetAxis("Horizontal"); // use the same axis that turns left/right
+//	    	transform.Rotate(0, Input.GetAxis ("Horizontal") * rotateSpeed, 0);
+//	    	var h = Input.GetAxis("Vertical"); // use the same axis that move back/forth
+//	    	var v = Input.GetAxis("Horizontal"); // use the same axis that turns left/right
 	
-//	    	transform.Rotate(0, Input.acceleration.x * rotateSpeed, 0);
-//	    	var h = Input.acceleration.y;
-//	    	var v = Input.acceleration.x;
+	    	transform.Rotate(0, Input.acceleration.x * rotateSpeed, 0);
+	    	var h = Input.acceleration.y;
+	    	var v = Input.acceleration.x;
 	    	
 	    	transform.localEulerAngles.x = -v*60; // forth/back banking first!
 	    }
