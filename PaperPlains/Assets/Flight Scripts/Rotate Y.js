de0ae20529fefee1e591171836d9825f02014727
Chong Li <hero_chong@qq.com>
2014-10-24 18:04:14 -0700

    Add the boundaries

--- PaperPlains/Assets/Flight Scripts/Rotate Y.js
+++ PaperPlains/Assets/Flight Scripts/Rotate Y.js
@@ -17,6 +17,11 @@ var endStar : Texture2D;
 var endinvincibleTime = 10000;
 var endspeedBoostTime = 10000;
 
+var maxX : float;
+var minX : float;
+var maxZ : float;
+var minZ : float;
+
 private var numRings : int;
 private var numRingsCounter : int;
 //private var score : int;
@@ -543,6 +548,19 @@ function Update() {
     // Keep plane from moving above max height
     if (rigidbody.position.y >= maxHeight) {
 		transform.position = Vector3 (rigidbody.position.x, maxHeight, rigidbody.position.z);
+	}
+	
+	if (rigidbody.position.z > maxZ) {
+		transform.position = Vector3 (rigidbody.position.x, rigidbody.position.y, maxZ);
+	}
+	if (rigidbody.position.z < minZ) {
+		transform.position = Vector3 (rigidbody.position.x, rigidbody.position.y, minZ);
+	}
+	if (rigidbody.position.x > maxX) {
+		transform.position = Vector3 (maxX, rigidbody.position.y, rigidbody.position.z);
+	}
+	if (rigidbody.position.x < minX) {
+		transform.position = Vector3 (minX, rigidbody.position.y, rigidbody.position.z);
 	}
 }
 
