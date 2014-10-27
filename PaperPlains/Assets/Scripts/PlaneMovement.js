b8bec8e1a447d07a4ce2a3db94893a8a2ae126df
alexadorsey <alexadorsey@hotmail.com>
2014-10-27 12:49:34 -0700

    Divided the files

---
+++ PaperPlains/Assets/Scripts/PlaneMovement.js
@@ -0,0 +1,45 @@
+﻿var LevelControls : LevelControls;
+private var init : float;
+private var rotateSpeed : float;
+
+//Set initial angle
+function Start () {
+	init = Input.acceleration.z;
+	rotateSpeed = 5.0;
+}
+
+function Update () {
+	if (!LevelControls.isGamePaused) {
+	
+	
+	
+		// Up & Down
+		var gap = Input.acceleration.z - init;
+		// Make smooth
+		if(Mathf.Abs( gap ) < 0.1){ // original: 0.05
+			transform.localEulerAngles.z = -5;  // left/right
+		} else {
+			if (gap * 90 >= 20)
+				transform.localEulerAngles.z = 20;
+			else if (gap * 90 <= -20)
+				transform.localEulerAngles.z = -20;
+			else			
+				transform.localEulerAngles.z = Mathf.Floor(gap * 90);
+		}
+		
+		
+		
+		// Left & Right
+		var controller : CharacterController = GetComponent(CharacterController);
+		//	transform.Rotate(0, Input.GetAxis ("Horizontal") * rotateSpeed, 0);
+		//	var h = Input.GetAxis("Vertical"); // use the same axis that move back/forth
+		//	var v = Input.GetAxis("Horizontal"); // use the same axis that turns left/right
+
+	    transform.Rotate(0, Input.acceleration.x * rotateSpeed, 0);
+	    var h = Input.acceleration.y;
+	    var v = Input.acceleration.x;
+
+		// forth/back banking
+		transform.localEulerAngles.x = -v*60; 
+	}
+}
---
+++ PaperPlains/Assets/Scripts/Tutorial.js
@@ -0,0 +1,98 @@
+﻿var LevelControls : LevelControls;
+var LevelDisplay : LevelDisplay;
+
+
+public var guidanceSetTimer:int;
+public var guidanceSetDistance:int;
+public var guidanceState: int;
+
+
+function Start () {
+
+	//Tutorial Use
+	LevelControls.isGuidanceShown = 0;
+	if (LevelControls.levelInt == 0) {
+		LevelDisplay.guidanceText = GameObject.Find("Guidance").guiText;
+		guidanceState = 1;//Start
+		guidanceSetTimer = 115;
+		guidanceSetDistance = 300;
+		LevelDisplay.guidanceText.fontSize = Mathf.Floor(Screen.dpi/7);
+		LevelDisplay.guidanceText.pixelOffset.y = 0;
+		LevelDisplay.guidanceText.color = Color.black;
+		LevelDisplay.guidanceText.text = "Welcome to the world of paper plain\n";
+	}
+
+}
+
+function Update () {
+	if (LevelControls.levelInt == 0) {
+		if(guidanceState > 0) {
+			if(rigidbody.position.x > guidanceSetDistance){
+				guidanceState++;				
+				guidanceSetDistance += 300;
+				if(guidanceState == 10){
+					guidanceSetDistance = 99999;
+				}
+				ShowGuidance();
+			}
+		}
+	}
+
+}
+
+
+function GuidanceTimer() {
+	if(guidanceSetTimer>0){
+		guidanceSetTimer --;
+	} else {
+		HideGuidance();
+	}
+}
+
+function HideGuidance() {
+	LevelDisplay.guidanceText.enabled = false;
+	LevelControls.UnPauseGame();	
+	LevelControls.isGuidanceShown = 0;	
+}
+
+
+
+//Show guidance in tutorial level
+function ShowGuidance(){
+	LevelDisplay.guidanceText.enabled = true;
+	guidanceSetTimer = 180;
+	LevelControls.PauseGame(); 
+	switch(guidanceState){
+			case 1:
+			LevelDisplay.guidanceText.text ="Welcome to the world of paper plain\n";
+			break;
+		case 2:
+			LevelDisplay.guidanceText.text = "Turn\n \nRotate your phone to turn left or right\n";
+			break;
+		case 3:
+			LevelDisplay.guidanceText.text = "Going up \n \nYou can tap screen to fly up\n though there's a maximum height";
+			break;
+		case 4:
+			LevelDisplay.guidanceText.text = "Targets: Go through the rings\n \nAs many rings as possible in the limited time!\n";
+			break;
+		case 5:
+			LevelDisplay.guidanceText.text = "Obstacles\n \nHitting obstacles will damage your paper plane\n(you lose heart)\n";
+			break;
+		case 6:
+			LevelDisplay.guidanceText.text = "Heart items\n \nHeart items can replenish your heart loss";
+			break;
+		case 7:
+			LevelDisplay.guidanceText.text = "Lightning items\n \nLightning items can speed you up for a short time";
+			break;
+		case 8:
+			LevelDisplay.guidanceText.text = "Shield items\n \nIt will give you a shield against annoying obstacles";
+			break;
+		case 9:
+			LevelDisplay.guidanceText.text = "Well done\n \nLet's go through all the rings in this level! ";
+			break;
+		case 10:
+			LevelDisplay.guidanceText.text = "Make sure you take all the rings before time is up. \nHappy flying!!!";
+			break;
+	}
+	LevelControls.isGuidanceShown = 1;
+}
