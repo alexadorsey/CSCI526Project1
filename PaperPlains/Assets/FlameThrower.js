dd0cfdcc6fd806a662fea83545c40b1837b584e5
Hrishi <hrishikesh.ghorpade@gmail.com>
2014-11-02 18:24:24 -0800

    added flame thrower

---
+++ PaperPlains/Assets/FlameThrower.js
@@ -0,0 +1,31 @@
+﻿#pragma strict
+
+var flamecollider : GameObject;
+var torch : ParticleSystem;
+var flag : boolean;
+
+function Start () {
+	flamecollider = GameObject.Find("Flamecollider");
+	torch.emissionRate = 0.0f;
+	flag = false;
+	InvokeRepeating("HandleFlame", 0, 2);
+}
+
+function FixedUpdate () {
+}
+
+function HandleFlame()
+{
+	if(flag)
+	{
+		flamecollider.GetComponent(Collider).enabled = false;
+		torch.emissionRate = 0.0f;
+		flag = false;
+	}
+	else
+	{
+		torch.emissionRate = 1500.0f;
+		flamecollider.GetComponent(Collider).enabled = true;
+		flag = true;
+	}
+}
